1b8b84d8500bd4c3e2f50732c8a51fca862e5a9d
fibbo <siebesiech@gmail.com>
2015-02-20 15:31:17 +0100

    some dictionary changes

--- HoughTransform.py
+++ HoughTransform.py
@@ -9,7 +9,8 @@ from Tools import *
 import pdb
 
 def createHistograms(data):
-  """ Fill a histogram for different radiuses with all the points for each circle center 
+  """ Fill a different histogram for circle centers with the distance between all points and the
+      respective center of the circle.
       :param double x, list of x coordinates
       :param double y, list of y coordinates
       :param list circle_centers, list of circle centers
@@ -18,24 +19,31 @@ def createHistograms(data):
 
   histograms = []
   for a, b in data['Center']:
-    h = Histogram(0.02, 0.29, 1.1, (a,b))
-    radiuses = calcRadius( data, a, b )
-    for radius in radiuses:
+    h = Histogram( 0.001, 0.29, 1.1, ( a, b ) )
+    res = calcRadius( data, a, b )
+    for radius in res['Radius']:
       h.addValue(radius)
     histograms.append(h)
   return histograms
 
+def calcRadius( data, a, b ):
+  res = { 'Radius' : [] }
+  x = data['x']
+  y = data['y']
+  for i in range( len( x ) ):
+      radius = math.sqrt( ( x[i] - a ) ** 2 + ( y[i] - b ) ** 2 )
+      res['Radius'].append( radius )
+
+  return res
 
 def findRadiusClusters(histograms):
   """ search through the histograms for a cluster. when a cluster is found we save the radius value
       of that cluster in a list and return it.
   :param Histogram histograms: list of histograms
-  :returns float radius: list of radiuses found
-  
-
+  :returns list of dicts for the different histograms with 'Center' and 'Radius' key.
   """
 
-  radiuses = []
+  res = []
   for h in histograms:
     clusters = []
     temp = []
@@ -49,9 +57,9 @@ def findRadiusClusters(histograms):
     result = {}
     result['Radius'] = temp
     result['Center'] = h.center
-    radiuses.append(result)
+    res.append( result )
 
-  return radiuses
+  return res
         
 
 def HoughTransform(data):
@@ -70,30 +78,23 @@ def HoughTransform(data):
       circle = plt.Circle(radius['Center'],i, fill=False)    
       fig.gca().add_artist(circle)
 
-  #fig.gca().add_artist(plt.Circle((-0.342,-0.994),0.719,fill=False,color='red'))
+  fig.gca().add_artist( plt.Circle( ( -0.342, -0.994 ), 0.719, fill = False, color = 'red' ) )
   fig.gca().add_artist(plt.Circle((-0.821,-0.656),0.563,fill=False,color='green'))
-  #fig.gca().add_artist(plt.Circle((-0.261,-0.328),0.892,fill=False,color='blue'))
+  fig.gca().add_artist( plt.Circle( ( -0.261, -0.328 ), 0.892, fill = False, color = 'blue' ) )
   plt.show()
 
 
-def calcRadius(data, a, b ):
-    r = []
-    x = data['x']
-    y = data['y']
-    for i in range(len(x)):
-        radius = math.sqrt((x[i]-a)**2 + (y[i]-b)**2)
-        r.append(radius)
 
-    return r
 
 if __name__ == '__main__':
   if len(sys.argv) < 2:
-      sys.exit( 'please provide file to be read')
+    sys.exit( 'please provide file to be read' )
   path = sys.argv[1]
   data = readFile(path)
   centers = [(-0.342,-0.994), (-0.821,-0.656), (-0.261,-0.328)]
   data['Center'] = centers
   HoughTransform(data)
+  print 'The End'
   
 
   
