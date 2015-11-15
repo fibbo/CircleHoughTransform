41d06ceb47daff1ddc27eb1ffce170c9771ef267
fibbo <siebesiech@gmail.com>
2015-11-15 16:16:04 +0100

    generate pkl

--- src/CircleDetection3Points.py
+++ src/CircleDetection3Points.py
@@ -8,7 +8,7 @@ import sys
 import itertools
 import pprint
 import pdb
-import pickle
+import cPickle as pickle
 import os
 
 import numpy as np
@@ -398,9 +398,7 @@ if __name__ == '__main__':
     found_circles = compareRings(entry, circles)
 
     pickle_data['foundRings'] = found_circles
-
-    pdb.set_trace()
-
+    pickle.dump( pickle_data, open(EVENTNUMBER+".pkl", 'wb'))
     print "Algorithm found %s/%s circles" % (len(found_circles),len(entry))
     plotData(x,y,circles,savePath=fileName)
 
