32efc6e04a079e2f73c78d5951fa3cb9cf5f0f56
fibbo <siebesiech@gmail.com>
2015-11-16 13:56:54 +0100

    better plots

--- src/visualizeData.py
+++ src/visualizeData.py
@@ -13,13 +13,26 @@ def convertTuplesToList( tuples ):
   return x,y
 
 def plotData( x,y, circles, savePath=None ):
+  colors = ['b','g','r','c','m','y']
+  font = {'weight' : 'normal',
+        'size'   : 18}
+  plt.rc('font', **font)
+  plt.rcParams['lines.linewidth'] = 1.2
+  my_dpi = 96
+  plt.figure(figsize=(800/my_dpi, 800/my_dpi), dpi=my_dpi)
+  plt.xlim(-0.5, 0.5)
+  plt.ylim(-0.5, 0.5)
+
+  
   fig = plt.gcf()
+  plt.xlabel('$x$ [m]')
+  plt.ylabel('$y$ [m]')
+  i = 0
   for circle in circles:
-    c = plt.Circle( circle['center'], circle['radius'], color='b',fill=False)
-    print circle['center']
-    plt.scatter( circle['center'][0], circle['center'][1], color='g', marker='v')
+    c = plt.Circle( circle['center'], circle['radius'], color=colors[i%6],fill=False)
+    i+=1
     fig.gca().add_artist(c)
-  plt.scatter(x,y, color='r', marker='o')
+  plt.scatter(x,y, color='k', marker='o')
   if savePath:
     fig.savefig(savePath)
   plt.show()
