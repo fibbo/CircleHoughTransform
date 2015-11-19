19fc7a61aebcdedf279313e5f19ec545c5939ab8
fibbo <siebesiech@gmail.com>
2015-11-19 16:30:12 +0100

    job script and pbs script updated

--- src/runEvents.sh
+++ src/runEvents.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-basedir="/disk/data3/lhcb/phi/circleHT"
-eventdir="/disk/data1/hep/che/HoughTransform/serie0/files"
-mkdir -p $basedir
-for event_id in {1..2}; do
+for event_id in {1..1000..10}; do
   qsub -l cput=02:00:00 -v event=$event_id submitData.pbs
 done
