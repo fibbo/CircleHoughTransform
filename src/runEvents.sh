94fd4342b326e2261a65a3de810911a6b82282df
fibbo <siebesiech@gmail.com>
2015-12-10 13:36:56 +0100

    40 files per job

--- src/runEvents.sh
+++ src/runEvents.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-<<<<<<< HEAD
 for event_id in {0..9999..40}; do
-=======
-for event_id in {0010..9999..10}; do
->>>>>>> origin/split_data
   qsub -l cput=02:00:00 -v event=$event_id submitData.pbs
 done
