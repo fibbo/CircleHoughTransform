6761085d590b7b384e6e959b0ca7276764c91f4e
fibbo <siebesiech@gmail.com>
2015-12-14 14:52:47 +0100

    merging

--- src/analysis.py
+++ src/analysis.py
@@ -3,9 +3,9 @@ import pdb
 import cPickle as pickle
 import numpy as np
 
-basePath = "/home/phi/workspace/CircleHT/analysis/run02"
+basePath = "/disk/data3/lhcb/phi/circleHT/splitData/run01/"
 directories = sorted(os.listdir(basePath))
-db = pickle.load(open("/home/phi/workspace/CircleHT/analysis/db.pkl", 'rb'))
+db = pickle.load(open("/home/hep/phi/CircleHoughTransform/src/db.pkl", 'rb'))
 
 def bulk():
   numberOfFakeRings = 0
@@ -28,10 +28,6 @@ def bulk():
           print e.code, e.message, os.path.join(path, afile), directory
       res = pickle.load( open(os.path.join(path,afile), 'rb') )
       db_rings = db[int(afile[:-4])]['rings']
-    # if afile == '0001.pkl':
-    #   print res
-    #   print db[int(afile[:-4])]
-    # 
       if len(res['fakeRings']):
        ifile.write(afile[:-4]+"\n")
       numberOfFakeRings += len(res['fakeRings'])
@@ -39,7 +35,7 @@ def bulk():
       numberOfTotalRings += len(db_rings)
       while (len(db_rings)):
         dbring = db_rings.pop()
-        if not any(np.linalg.norm(np.array(dbring['center'])-np.array(ring['center'])) < 0.25 and abs(dbring['radius'] - ring['radius']) < 0.10 for ring in res['foundRings']):
+        if not any(np.linalg.norm(np.array(dbring['center'])-np.array(ring['center'])) < 0.010 and abs(dbring['radius'] - ring['radius']) < 0.005 for ring in res['foundRings']):
           duplicateRings.append(dbring)
   print "number of missed rings: %s" % numberOfMissedRings
   print "number of fake rings: %s" % numberOfFakeRings
